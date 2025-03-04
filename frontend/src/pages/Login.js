fce7051bb2eb945e66db9157156b391392dfa591
Nikhil Shinde <160251430+nikkkhilllit@users.noreply.github.com>
2025-03-04 21:12:37 +0530

    backend url 1

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/auth/login', { email, password });
+      const response = await axios.post('https://devspacebackend-1vv6.onrender.com/auth/login', { email, password });
       console.log(response.data);
       localStorage.setItem('authToken', response.data.token);
       navigate('/dashboard');
